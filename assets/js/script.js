9d07dbebceb3fa757fc9b2a7967ae9ef799c32d7
67ujuj <vauclinjulien@gmail.com>
2023-05-02 08:49:17 +0200

    sauvegarde du matin 08h49

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+//exercice1
+
+// let i=1, n=100
+// while (i<n){
+//     document.write(i);
+//     i += 1;
+// }
+
+
+
+//exercice2
+
+// let i=100, n=1
+// while (i>n);
+//     if (i%2==0){
+//         document.write(i)
+//     }else{
+//         document.write("pas pair")
+//     }
+
+
+
+//exercice3
+
+// var nb1=3
+// var nb2=6
+// var nb3= nb1 + nb2
+// while(nb3<2023){
+//     nb3+nb2
+// }
+
+
+//exercice 4
+// let i=1, n=10
+// while (i<=n){
+//     document.write("Message ", i);
+//     document.write("<br>");
+//     i += 1;
+// }if (i=10){
+//     document.write("Message ", i, ", c'est le dernier message !");
+// }
