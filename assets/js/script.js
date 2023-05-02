a0421110231379593b6a50d5463b446b3c01e4ca
67ujuj <vauclinjulien@gmail.com>
2023-05-02 11:36:10 +0200

    Fin des exo foad

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,83 @@
 //exercice1
-
-// let i=1, n=100
-// while (i<n){
-//     document.write(i);
-//     i += 1;
+// let i = 1;
+// while (i <= 100) {
+//   console.log(i);
+//   i++;
 // }
-
-
 
 //exercice2
-
-// let i=100, n=1
-// while (i>n);
-//     if (i%2==0){
-//         document.write(i)
-//     }else{
-//         document.write("pas pair")
-//     }
-
-
+// let i = 100;
+// while (i > 1) {
+//   console.log(i);
+//   i -= 2;
+// }
 
 //exercice3
-
-// var nb1=3
-// var nb2=6
-// var nb3= nb1 + nb2
-// while(nb3<2023){
-//     nb3+nb2
+// var nb1 = 3;
+// var nb2 = 6;
+// while (nb1 + nb2 < 2023) {
+//   console.log(nb1 + nb2);
+//   nb2 += nb1 + nb2;
 // }
-
 
 //exercice 4
-// let i=1, n=10
-// while (i<=n){
-//     document.write("Message ", i);
-//     document.write("<br>");
-//     i += 1;
-// }if (i=10){
-//     document.write("Message ", i, ", c'est le dernier message !");
+// let i = 1;
+// while (i <= 10) {
+//   console.log("Message " + i);
+//   if (i == 10) {
+//     console.log("Message " + i + ", c'est le dixième message !");
+//   }
+//   i += 1;
 // }
+
+//exercice 5
+// var i = 1;
+// for (i; i <= 13; i++) {
+//   console.log(" Vendredi 13");
+// }
+
+//exercice 6
+// var i = 0;
+// for (i; i <= 99; i += 3) {
+//   console.log(i);
+// }
+
+//exercice 7
+// var i = 20;
+// for (i; i >= 0; i--) {
+//   if (i == 0) {
+//     console.log(i);
+//     console.log("finish");
+//   } else if (i == 20) {
+//     console.log("start");
+//     console.log(i);
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+//exercice 8
+// var i = 1;
+// for (i; i <= 100; i++) {
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     console.log("fizz");
+//   } else if (i % 5 == 0) {
+//     console.log("buzz");
+//   } else if (i % 3 == 0) {
+//     console.log("fizz buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+//exercice 9
+// var i = 1;
+// var mystring = "";
+// for (i; i <= 10; i++) {
+//   if (i != 10) {
+//     mystring += i + " - ";
+//   } else {
+//     mystring += i;
+//   }
+// }
+// console.log(mystring);
